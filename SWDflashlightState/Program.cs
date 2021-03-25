e651b11641147e943af6c89a9472cc0013d061f6
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-03-25 11:09:00 +0100

    Add project files.

---
+++ SWDflashlightState/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SWDflashlightState
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Flashlight
+    {
+        private bool IsOn=false;
+
+        public void ButtonPress()
+        {
+            switch (IsOn)
+            {
+                case true:
+                    TurnOff();
+                    IsOn = false;
+                    break;
+                case false:
+                    TurnOn();
+                    IsOff = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void TurnOff()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
